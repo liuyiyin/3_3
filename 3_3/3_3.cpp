33bd7b70ae7928ecd9f6f24f9828d7908f1e564a
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-03-13 14:44:34 +0800

    3_3

--- 3_3/3_3.cpp
+++ 3_3/3_3.cpp
@@ -2,10 +2,49 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
+using namespace std;
+using namespace cv;
+
+string window_name = "binaryMat";
+
+void threshod_Mat(int th, void* data)
+{
+	Mat src = *(Mat*)(data);
+
+	Mat dst;
+
+	threshold(src, dst, th, 255, 0);
+
+	imshow(window_name, dst);
+}
 
 int main()
 {
+	Mat srcMat;
+	Mat gryMat;
+	int lowTh = 30;
+	int maxTh = 255;
+
+	srcMat = imread("D:\\1.jpg");
+	if (!srcMat.data)
+	{
+		cout << "Í¼ÏñÔØÈëÊ§°Ü£¡" << endl;
+		return 0;
+	}
+
+	cvtColor(srcMat, gryMat, CV_BGR2GRAY);
+	imshow(window_name, gryMat);
+	createTrackbar("threshould",
+		            window_name,
+		            &lowTh,
+		            maxTh,
+		            threshod_Mat,
+		            &gryMat);
+    waitKey(0);
+   
     return 0;
 }
 
